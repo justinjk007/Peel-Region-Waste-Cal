87ac72bc5d16abcb1fb8b5f8d6a2bb9198800b8c
Justin K -Mac <justinjoseph0007@gmail.com>
2019-01-14 18:28:10 -0500

    Looks alright, now for processing

--- parse_data.py
+++ parse_data.py
@@ -1,5 +1,7 @@
 import sys
 import requests
+from bs4 import BeautifulSoup
+
 
 date_to_start_calendar = "2019-01-01"
 days=60
@@ -10,6 +12,12 @@ my_referer = sys.argv[1] # This is the query url with the Address, generated fro
 s = requests.Session()
 s.headers.update({'referer': my_referer})
 html = s.get(url)
-print html.content
-with open('Waste.html', 'w') as my_file:
-    my_file.writelines(html.content)
+
+soup = BeautifulSoup(html.content,"html.parser")
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+# Get remaining text data
+text = soup.get_text()
+with open('Waste.txt', 'w') as my_file:
+    my_file.writelines(text)
